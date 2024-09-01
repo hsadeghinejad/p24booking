ea94210d880802c363f9749436d95ab885b4e6a7
plasmic-app <github-action@actions-template-sync.noreply.github.com>
2024-09-01 01:34:28 +0000

    chore(template): merge template changes :up:
    Signed-off-by: plasmic-app <github-action@actions-template-sync.noreply.github.com>

--- src/pages/plasmic-host.tsx
+++ src/pages/plasmic-host.tsx
@@ -18,7 +18,6 @@ import { ApiRequest, apiRequestMeta } from "@/fragment/components/api-request";
 import { Slider, sliderMeta } from "@/fragment/components/slider";
 import { Chart, chartMeta } from "@/fragment/components/chart";
 import { Textarea, textareaMeta } from "@/fragment/components/textarea";
-import { Hamdast, hamdastMeta } from "@/hamdast/hamdast";
 
 export default function PlasmicHost() {
   return <PlasmicCanvasHost />;
@@ -37,4 +36,3 @@ registerComponent(Slider, sliderMeta);
 registerComponent(ApiRequest, apiRequestMeta);
 registerComponent(Chart, chartMeta);
 registerComponent(Textarea, textareaMeta);
-registerGlobalContext(Hamdast, hamdastMeta);
